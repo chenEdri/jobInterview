695dffbf80cf634fce5527e7ec8784db4e1ae96e
chen edri <chenedri22@gmail.com>
2021-03-21 21:12:55 +0200

    Updates

---
+++ precache-manifest.cbeb89a0f5e7c0db5c78139e95437136.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e8e88b998f1c3897c0c83901e150cb",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/index.html"
+  },
+  {
+    "revision": "f9c1792339c235ac1497",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/css/main.6bef75e8.chunk.css"
+  },
+  {
+    "revision": "ae14d6cdc0434d924471",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/js/2.0afa2c81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/js/2.0afa2c81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c1792339c235ac1497",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/js/main.b5fb3d4d.chunk.js"
+  },
+  {
+    "revision": "79bd11aa83c7f9afca6d",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/js/runtime-main.58289b21.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/%E2%80%9Chttp://chenEdri.github.io/jobInterview/static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
